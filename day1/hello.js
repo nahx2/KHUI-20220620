c8c74d893ac2100908ead69ee36a24cddaa4185c
061495 <104477618+061495@users.noreply.github.com>
2022-07-09 17:53:55 +0900

    first commit

---
+++ day1/hello.js
@@ -0,0 +1,8 @@
+let colors = require('colors');
+
+console.log(colors.rainbow('Hello NodeJS'));
+console.log(colors.green('Hello NodeJS'));
+console.log(colors.red('Hello NoedeJS'));
+let a = 5;
+console.log(a);
+console.log(a);
---
+++ day1/main.js
@@ -0,0 +1,11 @@
+function add(a,b){
+    return a+b;
+}
+
+console.log(add(1,2));
+console.log(add(2,3));
+let colors = require('colors');
+console.log(colors.rainbow('hello'));
+console.log(colors.green('hello NodeJS'));
+
+console.log('test');
