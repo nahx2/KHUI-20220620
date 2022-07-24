9541981e534893ec761cd84509131c3a875885f1
nahx2 <nax2boo@gmail.com>
2022-07-24 21:27:02 +0900

    220724

---
+++ js-day8/ze5.js
@@ -0,0 +1,40 @@
+const itemList = [];
+
+const btn_add = document.querySelector(".btn_add");
+btn_add.addEventListener("click", addList); //addBtn.onclick = addList
+
+function addList(){
+    // 텍스트 필드 내용을 가져오기 
+    const item = document.querySelector(".footer_input").value;
+    if (item != null){
+        itemList.push(item);  //itemList  끝에 item 내용 추가 
+        document.querySelector(".footer_input").value = "";
+        document.querySelector(".footer_input").focus();
+    }
+    console.log(item);
+    showList();
+}
+
+function showList(){
+  const list = "<ul>";
+    console.log(itemList);
+    console.log(itemList.length);
+    for (let i =0; i < itemList.length ; i++){
+        list += "<li>"+  itemList[i]  + "<span class='close' id=" + i + ">X</span></li>";
+    }
+    list += "</ul>";
+    document.querySelector('.itemList').innerHTML = list;  //list 내용 표시 
+
+    // 삭제버튼을 변수로 저장 
+    const remove = document.querySelectorAll(".close");
+
+    for (let i=0; i < remove.length; i++){
+        remove[i].addEventListener("click", removeList); //요소를 클릭하면 removeList()실행
+    }
+}
+
+function removeList(){
+  const id = this.getAttribute("id"); //클릭한 삭제 버튼의 id값을 가져온다 .
+    itemList.splice(id, 1);
+    showList();
+}
